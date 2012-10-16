cc93070349409fa0b77c3b29a784344694b4cd80
Chris Sanders <zidane@outlook.com>
2012-10-16 19:01:25 -0400

    Update for TeraBullet changes. Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spout/engine/world/collision/SpoutPhysicsWorld.java
+++ src/main/java/org/spout/engine/world/collision/SpoutPhysicsWorld.java
@@ -29,7 +29,7 @@ package org.spout.engine.world.collision;
 import javax.vecmath.Vector3f;
 
 import com.bulletphysics.collision.shapes.CollisionShape;
-import com.bulletphysics.collision.shapes.voxel.CollisionSnapshot;
+import com.bulletphysics.collision.shapes.voxel.VoxelCollisionInfo;
 import com.bulletphysics.collision.shapes.voxel.VoxelPhysicsWorld;
 
 import org.spout.api.material.BlockMaterial;
@@ -49,17 +49,17 @@ public class SpoutPhysicsWorld implements VoxelPhysicsWorld {
 		this.simulation = simulation;
 	}
 	@Override
-	public CollisionSnapshot getCollisionShapeAt(int x, int y, int z) {
+	public VoxelCollisionInfo getCollisionShapeAt(int x, int y, int z) {
 		final SpoutBlock block = (SpoutBlock) simulation.getBlock(x, y, z, null);
-		return new SpoutCollisionSnapshot(block.getMaterial(), block.getPosition());
+		return new SpoutVoxelCollisionInfo(block.getMaterial(), block.getPosition());
 	}
 
-	private static class SpoutCollisionSnapshot implements CollisionSnapshot {
+	private static class SpoutVoxelCollisionInfo implements VoxelCollisionInfo {
 		private final boolean isColliding, isBlocking;
 		private final CollisionShape shape;
 		private final Vector3 position;
 
-		public SpoutCollisionSnapshot(BlockMaterial material, Vector3 position) {
+		public SpoutVoxelCollisionInfo(BlockMaterial material, Vector3 position) {
 			this.shape = material.getCollisionShape();
 			this.isColliding = shape != null && material.isInteractable();
 			this.isBlocking = shape != null && !material.isPenetrable();
